c797a8d97fa4355490de97c3e6ce05cb5c903dd1
Sanyam-bansal19 <sanyam19bansal@gmail.com>
2025-02-15 13:21:46 +0530

    Q42_Associatedarray.php

--- Q42_Associatedarray.php
+++ Q42_Associatedarray.php
@@ -7,8 +7,8 @@
     echo "Salary of Zara is ".  $salaries['zara']. "<br>";
      
     $salaries['roshan'] = "high";
-    $salaries['qadir'] = "medium";
-    $salaries['twinkle'] = "low";
+    $salaries['twinkle'] = "medium";
+    $salaries['zara'] = "low";
      
     echo "Salary of Roshan is ". $salaries['roshan'] . "<br>";
     echo "Salary of Twinkle is ".  $salaries['twinkle']. "<br>";
