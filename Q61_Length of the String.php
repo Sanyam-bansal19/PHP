53c0683e102040ab4b51d0a35efb4945dfd3c9a5
Sanyam-bansal19 <sanyam19bansal@gmail.com>
2025-03-21 14:44:37 +0530

    Add files via upload

---
+++ Q61_Length of the String.php
@@ -0,0 +1,8 @@
+<?php
+
+    $my_str = "Welcome to Sanyam's GitHub";
+     
+    // Calculating and displaying string length
+    echo strlen($my_str);
+    echo"<br>This code is executed by Sanyam Bansal!";
+?>
