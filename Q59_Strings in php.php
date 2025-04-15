25be4db88b8f7a02edf09dbca9189836d46f8dfc
Sanyam-bansal19 <sanyam19bansal@gmail.com>
2025-04-15 16:24:52 +0530

    Add files via upload

---
+++ Q59_Strings in php.php
@@ -0,0 +1,11 @@
+<?php
+
+    $my_str = 'World';
+    echo "Hello, $my_str!<br>"; 
+    echo 'Hello, $my_str!' . "<br>"; 
+     
+    echo 'Hello\tWorld!'; 
+    echo "<br>Hello\tWorld!<br>"; 
+    echo 'I\'ll be back'; 
+    echo"<br>This code is executed by Sanyam Bansal";
+?>
